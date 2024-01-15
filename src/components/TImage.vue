c450eda145934d59fa22d03e18347c10e4d1d1fe
Nerea Fontecha <nerea@landt.co>
2024-01-15 15:24:44 +0100

    image

--- src/components/TImage.vue
+++ src/components/TImage.vue
@@ -1,23 +1,43 @@
 <template>
     <div>
         <img 
-        :class="className" 
-        :src="src" 
-        :alt="alt"
-        :width="width"
-        :height="height"
-        :style="style" 
-        :srcset="srcset"
-    />
+            v-if="isLazy"
+            :class="className" 
+            :data-src="src" 
+            :src="getPlaceholderImage()"
+            :alt="getAltName(src)"
+            :width="width"
+            :height="height"
+            :style="style" 
+            :srcset="srcset"
+            :sizes="getSizes()"
+        />
+        <img 
+            v-if="!isLazy"
+            :class="className" 
+            :src="src" 
+            :alt="getAltName(src)"
+            :width="width"
+            :height="height"
+            :style="style" 
+            :srcset="srcset"
+            :sizes="getSizes()"
+        />
     </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
+
 const props = defineProps({
-    className: {
+    image: {
         type: String,
     },
-    src: {
+    isLazy : {
+        type : Boolean,
+        default : false
+    },
+    className: {
         type: String,
     },
     alt: {
@@ -29,11 +49,64 @@ const props = defineProps({
     height: {
         type: String,
     },
-    style: {
-        type: String,
-    },
     srcset: {
         type: String,
     },
+    showAspectRatio: {
+        type : Boolean,
+        default : false
+    },
 })
+const style = ref("16 / 9")
+
+onMounted(() => {
+  if(props.isLazy){
+
+  }
+  if(props.width && props.height){
+    style.value = `${props.width} / ${props.height}`;
+  }
+  if(props.showAspectRatio){
+
+  }
+  if(props.sizes){
+    this.getSizes();
+  }
+})
+
+const getAltName = () =>{
+    if(props.alt)
+        return props.alt;
+    return props.image.split('/').pop();
+}
+
+const getPlaceholderImage = ( ) =>{
+    return ""
+}
+
+const getSizes = () =>{
+    var sizeResult = '';
+    switch (props.sizes) {
+        case 'large':
+            sizeResult = '100vw';
+            break;
+        case 'medium':
+            sizeResult = '(max-width: 810px) 50vw, 100vw';
+            break;
+
+        case 'small':
+            sizeResult = '(max-width: 810px) 33vw, 95vw';
+            break;
+
+        case '':
+            sizeResult = '95vw';
+            break;
+        
+        default:
+            sizeResult = props.sizes;
+            break;
+    }
+    return sizeResult;
+}
+
 </script>
